8c9bf6a69268b42ef712aa55f2d3f8552258bdf1
Somashekhar <somashekharm4159@gmail.com>
2023-02-27 22:57:19 +0530

    Iterating Elements of ArrayList

--- uis/List/ArrayList01.java
+++ uis/List/ArrayList01.java
@@ -12,11 +12,27 @@ public class ArrayList01 {
 						a1.add(30);
 						a1.add(40);
 						a1.add(50);
-					ArrayList01.printingListElements(a1);
-	System.out.println("---The End---");
-	
+						
+/*
+ * 1. WAP to create a new ArrayList, add some colours (String) and PRINT out the collection.
+ */
+	//	ArrayList01.printingListElements(a1);
+		
+/*
+ * 2. WAP to ITERATE through all elements & MULTIPLY BY 2 in an ArrayList.
+ */
+		ArrayList01.iteratingElements(a1);
 	}
+	
+	
 	public static void printingListElements(List list) {
 		System.out.println(list);
+	}
+	
+	public static void iteratingElements(List list) {
+		for(int i = 0 ; i < list.size() ; i++) {
+			int num = (int)list.get(i);
+			System.out.print(2*num+" ");
+		}
 	}
 }

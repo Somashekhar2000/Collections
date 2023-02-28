f274e18fab79c3c01c199c712188c9927d04df62
Somashekhar <somashekharm4159@gmail.com>
2023-02-28 16:14:49 +0530

    Appending the specified element to the end of a LinkedList

--- uis/List2/LinkedListPracticals.java
+++ uis/List2/LinkedListPracticals.java
@@ -1,10 +1,27 @@
 package com.uis.List2;
 
+import java.util.LinkedList;
+
 public class LinkedListPracticals {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		LinkedList a1 = new LinkedList();
+/*
+ * 1. WAP to append the specified element to the end of a LinkedList.
+ */
+		a1.add("Red");
+		a1.add("Green");
+		a1.add("Black");
+		a1.add("White");
+		a1.add("Pink");
+		a1.add("Yellow");
+		
+		System.out.println(a1);
+		
 	}
+	
+	
 
 }

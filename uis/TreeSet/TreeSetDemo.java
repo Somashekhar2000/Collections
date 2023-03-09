51293774890d70259814928035f9ee845a523d48
Somashekhar <somashekharm4159@gmail.com>
2023-03-09 23:02:14 +0530

    Printing elements using Iterator

--- uis/TreeSet/TreeSetDemo.java
+++ uis/TreeSet/TreeSetDemo.java
@@ -1,5 +1,6 @@
 package com.uis.TreeSet;
 
+import java.util.Iterator;
 import java.util.TreeSet;
 
 public class TreeSetDemo {
@@ -15,7 +16,22 @@ public class TreeSetDemo {
 		set1.add("Black");
 		set1.add("Green");
 		
-		System.out.println("Created new TreeSet : "+set1);
+	//	System.out.println("Created new TreeSet : "+set1);
+		
+//2. WAP to iterate through all elements in a TreeSet.
+		TreeSetDemo.iteratingTreeSet(set1);
+		
 	}
+	
+	public static void iteratingTreeSet(TreeSet set1) {
+		Iterator t1 = set1.iterator();
+		System.out.println("Printing elements using Iterator");
+		System.out.print("[");
+		while(t1.hasNext()) {
+			System.out.print(t1.next()+",");
+		}
+		System.out.println("]");
+	}
+	
 
 }

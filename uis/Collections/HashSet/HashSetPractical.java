a470e6ee24cd0dbfc3f58cc573408ccc07adad11
Somashekhar <somashekharm4159@gmail.com>
2023-03-09 10:49:15 +0530

    Appending the specified element to the end of a HashSet.

--- uis/Collections/HashSet/HashSetPractical.java
+++ uis/Collections/HashSet/HashSetPractical.java
@@ -1,6 +1,7 @@
 package com.uis.Collections.HashSet;
 
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class HashSetPractical 
 {
@@ -9,6 +10,13 @@ public class HashSetPractical
 	{
 		HashSet a1 = new HashSet<>();
 		
+//1. WAP to append the specified element to the end of a HashSet.
 		
-	}
+		a1.add("Red");
+		a1.add("blue");
+		a1.add("black");
+		a1.add("green");
+		a1.add("white");
+		
+
 }

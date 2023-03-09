a9e8297e2d70cb798894653001f3b789f4e33107
Somashekhar <somashekharm4159@gmail.com>
2023-03-09 22:52:06 +0530

    Creating new TreeSet

--- uis/TreeSet/TreeSetDemo.java
+++ uis/TreeSet/TreeSetDemo.java
@@ -1,10 +1,21 @@
 package com.uis.TreeSet;
 
+import java.util.TreeSet;
+
 public class TreeSetDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//1. WAP to create a new TreeSet, add some colours (string) and print out the TreeSet.
+		TreeSet set1 = new TreeSet<>();
 		
+		set1.add("Red");
+		set1.add("White");
+		set1.add("Pink");
+		set1.add("Yellow");
+		set1.add("Black");
+		set1.add("Green");
+		
+		System.out.println("Created new TreeSet : "+set1);
 	}
 
 }

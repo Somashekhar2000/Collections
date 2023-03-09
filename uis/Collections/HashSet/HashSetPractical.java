58172d80e53caffac702a2b309cd3da5f041f435
Somashekhar <somashekharm4159@gmail.com>
2023-03-09 10:36:27 +0530

    Hashset Exercies

---
+++ uis/Collections/HashSet/HashSetPractical.java
@@ -0,0 +1,14 @@
+package com.uis.Collections.HashSet;
+
+import java.util.HashSet;
+
+public class HashSetPractical 
+{
+
+	public static void main(String[] args) 
+	{
+		HashSet a1 = new HashSet<>();
+		
+		
+	}
+}
